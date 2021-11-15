5fc21e8d3e9b575ba949e435a0c6bcc07ef0bb4f
Carlos <carlostapiaman@gmail.com>
2021-11-15 16:53:05 +0100

    Defined more variables

--- dipper_robot/dipper_robot.ino
+++ dipper_robot/dipper_robot.ino
@@ -26,6 +26,9 @@
 #define LED 9         // Operational LED 
 #define homeSwitch 13  // Reference Position signal (Before it was 3 but pin broke)
 
+// define a variable for the reference position point
+long initial_homing=-1;  // Used to Home Stepper at startup
+
 // Define the stepper motor and the pins it will use, and its setup
 AccelStepper stepper( AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 double mode = 1; //the stepping mode {1, 2, 4, 8, 16, 32}
@@ -34,6 +37,35 @@ double mode = 1; //the stepping mode {1, 2, 4, 8, 16, 32}
 
 // Define some communication variables
 int incomingByte;      // a variable to read incoming serial data into
+
+// Define variables for Kinematik/Dynamics
+const double Ulin = 25;          //linear transmition power screw IGUS [mm/turn]
+const int U = 1;             //Gear ratio: input/output
+//const double Utri = 1;     //correction for trinema silent driver ON/OFF/OFF (check if necessary)
+const double H_max = 425;     //maximum height in mm(upper physical limit)
+const double H_min = 260;      //minimum height in mm(lower physical limit)
+double H;                     //A variable to store the current height in mm
+double kres = Ulin/(MOTOR_FULL_STEPS*mode*U); //[mm/steps] steps to mm. (Resolution)
+
+//defined default motion parameters
+int SPD_lin_def = 30; //linear speed in mm/s
+double ACC_time_def = 500; //[ms] the time it takes to accelerate to max speed
+int startDelay_def = 200; //[ms] the time delay before an operation begins
+int dipTime_def = 500; //[ms] the time it stays on the targeted position (Dipping)
+int homingSPD = MOTOR_STEPS*1; //Homing speed in steps per second
+
+//used motion parameters. These variables are actively used.
+long SPD_lin; //linear speed in mm/s
+double ACC_time;  //[ms] the time it takes to accelerate to the defined speed
+int startDelay;   //[ms] 
+int dipTime;      //[ms] 
+
+//Unit conversion variables (between mm and steps)
+int SPD_steps;// = SPD_lin_def/kres; // Speed in steps per second
+int ACC_steps;// = SPD_lin_def/(kres*ACC_time_def*0.001); // Acceleration in steps per second per second
+
+// target position in the operation
+double targetPos;
 
 
 
