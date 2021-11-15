77c4786c40a5cba0674d812bbb3f72ef0f0208a6
Carlos <carlostapiaman@gmail.com>
2021-11-15 16:44:26 +0100

    Added code description and libraries

--- dipper_robot/dipper_robot.ino
+++ dipper_robot/dipper_robot.ino
@@ -1,3 +1,25 @@
+/**************************************************************************/
+/* Linear motion within two boundaries                                    */
+/* Arduino source code for stepper motor control                          */ 
+/*                                                                        */
+/*                                                                        */
+/* Hardware                                                               */
+/* Board: Arduino Uno                                                     */
+/* Periphery: HC05 Bluetooth                                              */
+/* Stepper Driver: DM320T                                                 */
+/* Arduino IDE Version 1.8.12                                             */
+/* Library used: "AccelStepper" by Mike McCauley V 1.59.0                 */
+/*                                                                        */
+/* Version: 1.0                                                           */
+/* Stand: 15.11.2021                                                      */
+/* Author: Carlos Tapia Mancera                                           */
+/**************************************************************************/
+
+#include <Arduino.h>
+#include <AccelStepper.h>
+//https://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html#a176c5d2e4c2f21e9e92b12e39a6f0e67
+
+
 void setup() {
   // put your setup code here, to run once:
 
